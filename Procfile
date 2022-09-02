affb2b9946a253d8947929a32cc886a5d54419ee
Apirat tasanawongwara <apirat.t05@gmail.com>
2022-09-03 01:28:40 +0700

    runtime

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: kratom app:app
--- app.py
+++ app.py
@@ -51,7 +51,6 @@ app = Flask(__name__)
 
 @app.errorhandler(HTTPException)
 def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
     # start with the correct headers and status code from the error
     response = e.get_response()
     # replace the body with JSON
